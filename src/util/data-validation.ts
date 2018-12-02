79345095b818b30f5583d333f6c1a45459e170d7
Victorien Elvinger <victorien@elvinger.fr>
2018-12-02 17:09:21 +0100

    refactor: add @internal tags

--- src/util/data-validation.ts
+++ src/util/data-validation.ts
@@ -1,7 +1,12 @@
+// Copyright (c) 2018 Victorien Elvinger
+//
+// Licensed under the zlib license (https://opensource.org/licenses/zlib).
 
+/** @internal */
 export type NonFunctionNames <T> =
 { [k in keyof T]: T[k] extends Function ? never : k }[keyof T]
 
+/** @internal */
 export type Unknown <T> = { [k in NonFunctionNames<T>]?: unknown }
 
 /**
@@ -10,6 +15,7 @@ export type Unknown <T> = { [k in NonFunctionNames<T>]?: unknown }
  * `isObject<{ p: number }>(x) && typeof x.p === "number"`
  * enables to test if x is conforms to `{ p: number }`.
  *
+ * @internal
  * @param x
  * @param Is `x' a non-null object?
  */
