a84545ea533f7f7e13b0e3b642ce084d334a3838
Victorien Elvinger <victorien@elvinger.fr>
2019-01-02 18:24:15 +0100

    fix: export kybos

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export { Random } from "./core/random"
 export { RandomStream } from "./core/random-stream"
 
 export { alea, AleaState } from "./impl/alea"
+export { kybos, KybosState } from "./impl/kybos"
 export { uhe, UheState } from "./impl/uhe"
 
 export { u32, i32, i54, f64, fract32, fract53 } from "./util/number"
